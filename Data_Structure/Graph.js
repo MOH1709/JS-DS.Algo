6824359bbb35846e7fcff03c832f36ab9a343938
MOH1709 <ahirwalmohit3216@gmail.com>
2021-10-14 11:55:49 +0530

    "Added bfs in graph"

--- Data_Structure/Graph.js
+++ Data_Structure/Graph.js
@@ -1,7 +1,10 @@
+const Queue = require("./Queue");
+
 class Graph {
   constructor() {
     let map = new Map();
 
+    // Add undirected nodes in graph
     this.addEdge = (nodeName, names = []) => {
       map.has(nodeName) || map.set(nodeName, []);
 
@@ -13,6 +16,25 @@ class Graph {
       }
     };
 
+    // Breath first search
+    this.BFS = (from) => {
+      let isVisited = new Map();
+      let q = new Queue(from);
+
+      isVisited.set(from);
+      while (q.isEmpty() === false) {
+        var connArr = map.get(from);
+
+        for (var node of connArr) {
+          if (isVisited.has(node) === false) {
+            isVisited.set(node);
+            q.enqueue(node);
+          } else console.log(q.dequeue());
+        }
+      }
+    };
+
+    //print graph with all edge connections
     this.print = () => {
       let keysArr = map.keys();
 
@@ -28,3 +50,4 @@ let g1 = new Graph();
 g1.addEdge("a", [1, 3, "b", "s"]);
 g1.addEdge("b", [1, 3, "s", "b"]);
 g1.print();
+g1.BFS("a");
