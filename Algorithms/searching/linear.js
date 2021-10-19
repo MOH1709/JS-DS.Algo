4b4dbddd92824a993206e1cb9cd1424ff89b6f26
MOH1709 <ahirwalmohit3216@gmail.com>
2021-10-19 10:40:06 +0530

    added linear search

---
+++ Algorithms/searching/linear.js
@@ -0,0 +1,9 @@
+let arr = [5, 9, 7, 4, 6, 8, 1, 3, 2];
+
+function linear(arr = [], target) {
+  return arr.reduce((init, data, i) => {
+    return data === target ? i : init;
+  }, -1);
+}
+
+console.log(linear(arr, 2));
